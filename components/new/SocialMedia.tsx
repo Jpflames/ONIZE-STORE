884e09d507c27f80900d29256433e4fecdb0274b
Jpflames <israeljohnpaul28@gmail.com>
2026-04-29 23:55:42 +0100

    updated social media icons

--- components/new/SocialMedia.tsx
+++ components/new/SocialMedia.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Facebook, Github, Linkedin, Slack, Youtube } from "lucide-react";
+import { Facebook, Github, Linkedin, Slack, Youtube, Instagram  } from "lucide-react";
 import React from "react";
 import {
   Tooltip,
@@ -19,28 +19,33 @@ interface Props {
 const socialLink = [
   {
     title: "Youtube",
-    href: "https://www.youtube.com/@reactjsBD",
+    href: "https://youtube.com/@onizecrochets?si=NMqp5J4wSB2YLkLf",
     icon: <Youtube className="w-5 h-5" />,
   },
   {
     title: "Github",
-    href: "https://www.youtube.com/@reactjsBD",
+    href: "",
     icon: <Github className="w-5 h-5" />,
   },
   {
     title: "Linkedin",
-    href: "https://www.youtube.com/@reactjsBD",
+    href: "",
     icon: <Linkedin className="w-5 h-5" />,
   },
   {
     title: "Facebook",
-    href: "https://www.youtube.com/@reactjsBD",
+    href: "",
     icon: <Facebook className="w-5 h-5" />,
   },
   {
     title: "Slack",
-    href: "https://www.youtube.com/@reactjsBD",
+    href: "",
     icon: <Slack className="w-5 h-5" />,
+  },
+  {
+    title: "Instagram",
+    href: "https://www.instagram.com/onizecrochet?igsh=MzNveTNneTk2MXIw",
+    icon: <Instagram className="w-5 h-5" />,
   },
 ];
 
